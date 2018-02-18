4606c990b942963a6e21d7cbc1954db0f6055d78
Annie Xu <xuyangannie2001@gmail.com>
2018-02-18 15:32:03 -0500

    Unplug

---
+++ GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  Unplug
+//
+//  Created by yang xu on 2/18/18.
+//  Copyright © 2018 Object Coder. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Is Headphone Plugged In/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Unplug
+//
+//  Created by yang xu on 2/18/18.
+//  Copyright © 2018 Object Coder. All rights reserved.
+//
+
+import Foundation
